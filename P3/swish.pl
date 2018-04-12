e01e3e760cd6c43d504e6abf2c4e34945dd0a7cb
luiscarabe <luiscarabe@users.noreply.github.com>
2018-04-12 21:41:05 +0200

    4-2 y 5 acabados

--- P3/swish.pl
+++ P3/swish.pl
@@ -15,3 +15,11 @@ insert([A-X], [B-Y|Rs], L3) :- not(X<Y), insert([A-X], Rs, L4), concatena([B-Y],
 elem_count(_,[],0).
 elem_count(X,[X|Rs],Xn):- elem_count(X, Rs, Xm), Xn is(1+Xm).
 elem_count(X,[Y|Rs],Xn):- X\=Y, elem_count(X, Rs, Xn).
+
+list_count([],_,L) :- [] = L.
+list_count([X|Rx],Y,L) :- list_count(Rx, Y, M), elem_count(X,Y,Rs), concatena([X-Rs],M, A), A=L.
+
+sort_list([],[]).
+sort_list([A-X|Rs], L) :- sort_list(Rs, M), insert([A-X], M, R), R=L.
+
+
