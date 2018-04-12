ff9e81224328ed5ee1ba2cf57967148615f101bf
Nuriacs <nuriacszazuar@gmail.com>
2018-04-12 17:04:38 +0200

    3 y 4 hechooos

--- P3/swish.pl
+++ P3/swish.pl
@@ -7,3 +7,11 @@ concatena([X|L1], L2, [X|L3]) :- concatena(L1, L2, L3).
 
 invierte([], []).
 invierte([X|Rx], Y) :- invierte(Rx,M), concatena(M, [X], Y).
+
+insert([X], [], [X]).
+insert([A-X], [B-Y|Rs], L3) :- X<Y, concatena([A-X], [B-Y|Rs], L3).
+insert([A-X], [B-Y|Rs], L3) :- not(X<Y), insert([A-X], Rs, L4), concatena([B-Y], L4, L3).
+
+elem_count(_,[],0).
+elem_count(X,[X|Rs],Xn):- elem_count(X, Rs, Xm), Xn is(1+Xm).
+elem_count(X,[Y|Rs],Xn):- X\=Y, elem_count(X, Rs, Xn).
