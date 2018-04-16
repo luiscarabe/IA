dfcd1f7ea64d2718e57de5ec924a61729134c543
luiscarabe <luiscarabe@users.noreply.github.com>
2018-04-17 00:30:39 +0200

    Comento el 7 y me pongo con la memoriaaa

--- P3/2302_p3_05.pl
+++ P3/2302_p3_05.pl
@@ -46,15 +46,38 @@ elem_count(_,[],0).
 elem_count(X,[X|Rs],Xn):- elem_count(X, Rs, Xm), Xn is(1+Xm).
 elem_count(X,[Y|Rs],Xn):- X\=Y, elem_count(X, Rs, Xn).
 
-%4.2
+% 4.2 - list_count(X,Y,Z)
+% Predicado que usamos para comprobar si una lista (Z) contiene las ocurrencias de
+% los elementos de la lista X en la lista Y en forma de par
+% Input : 	X - lista con los elementos que buscamos en Y
+% 			Y - lista de elementos
+% 			Z - lista resultante de los pares elementoDeX-#ocurrenciasEnY
 
+% Caso base: si X esta vacia, la lista resultante tambien
 list_count([],_,[]).
+
+% Caso recursivo:
+% Dividimos la primera lista en su primer elemento (X) y el resto, llamamos a list_count con 
+% este resto, el resultado sera M. Ahora contamos con ayuda de elem_count, las veces que aparece
+% el elemento X en la segunda lista, este resultado sera Rs. 
+% Ahora concatenamos el par X-Rs con M, este debera ser el resultado de la tercera lista
 list_count([X|Rx],Y,L) :- list_count(Rx, Y, M), elem_count(X,Y,Rs), concatena([X-Rs],M, L).
 
-%5
+% 5 - sort_list(X,Y)
+% Predicado que usamos para comprobar si una lista contiene los pares de elementos de otra
+% en orden.
+% Input : 	X - lista con los pares de elementos
+% 			Y - lista que deberia contener los elementos de X ordenados
 
+% Caso base: Una lista vacia esta ordenada siempre
 sort_list([],[]).
+
+% Caso recursivo:
+% Dividimos la primera lista en su primer par y el resto, llamamos a sort-list con 
+% este resto e insertamos en el resultado el primer par, 
+% esta inserción será el resultado en la segunda lista
 sort_list([A-X|Rs], L) :- sort_list(Rs, M), insert([A-X], M, L).
+
 
 %6
 
@@ -62,12 +85,18 @@ build_tree([], nil).
 build_tree([A], tree(A, nil, nil)).
 build_tree([A-_|Rs], tree(1, X, M) ) :- Rs \= [], build_tree(Rs, M), X = tree(A, nil,nil).
 
-%7
-%7.1
+% 7
 
+% 7.1
+% Predicado que usamos para comprobar si una lista contiene los pares de elementos de otra
+% en orden.
+% Input : 	X - lista con los pares de elementos
+% 			Y - lista que deberia contener los elementos de X ordenados
+
+% Caso base: Una lista vacia esta ordenada siempre
 encode_elem(A, [0], tree(A, nil, nil)). % caso base extraordinario
-
 encode_elem(A, [0], tree(1, tree(A, nil, nil),_)). 
+
 encode_elem(A, [1], tree(1,_, tree(A, nil,nil))). % Caso ultima rama de arbol
 encode_elem(A, X, tree(1, tree(_, nil, nil), Rs)) :- Rs \= tree(A,nil,nil), encode_elem(A,M,Rs), concatena([1], M, X).
 
