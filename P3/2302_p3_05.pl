32156bb248ed468c37d6ad029a7c57e342c373bc
Nuriacs <nuriacszazuar@gmail.com>
2018-04-19 15:43:19 +0200

    Add files via upload
    luis es tontito, pero le quiero jejee

--- P3/2302_p3_05.pl
+++ P3/2302_p3_05.pl
@@ -33,16 +33,37 @@ invierte([], []).
 % este será el resultado en la segunda lista
 invierte([X|Rx], Y) :- invierte(Rx,M), concatena(M, [X], Y).
 
-%3
+% 3 - insert(X-P,L,R)​
+% Predicado que usamos para comprobar insertar elementos en una lista de manera ordenada.
+% Input : 	X-P - elemento a insertar
+% 			L - lista ordenada donde insertamos
+%			R - lista con los elementos insertados ordenadamente
 
+% Caso base: Insertar un elemento en una lista vacia
 insert([X], [], [X]).
+
+% Caso recursivo:
+% En caso de que la posicion(P) del elemento a insertar sea menor o igual que el elemento 
+% de la lista a analizar, concatenamos la lista detras del elemento.
+% En caso contrario llamamos de nuevo a insertar con el resto de la lista concatenando 
+% con delante el primero de la lista que ya hemos comparado.
 insert([A-X], [B-Y|Rs], L3) :- X=<Y, concatena([A-X], [B-Y|Rs], L3).
 insert([A-X], [B-Y|Rs], L3) :- not(X=<Y), insert([A-X], Rs, L4), concatena([B-Y], L4, L3).
 
 %4
-%4.1
+% 4.1 - elem_count(X,L,Xn)
+% Predicado que cuenta la cantidad de veces que aparece un elemento en una lista.
+% Input : 	X - elemento a contar
+% 			L - lista de elementos
+%			Xn - número de veces que aparece el elemento
 
+% Caso base: Cualquier elemento esta 0 veces en una lista vacia
 elem_count(_,[],0).
+% Caso recursivo:
+% En caso de que la encontremos el mismo elemento en la lista sumamos 1 al numero de veces
+% y volvemos a llamar de manera recursiva con el resto de la lista
+% En caso de que no sea el mismo mantenemos el mismo numeor de veces, unicamente llamando 
+% recursivamente con el resto.
 elem_count(X,[X|Rs],Xn):- elem_count(X, Rs, Xm), Xn is(1+Xm).
 elem_count(X,[Y|Rs],Xn):- X\=Y, elem_count(X, Rs, Xn).
 
@@ -79,9 +100,16 @@ sort_list([],[]).
 sort_list([A-X|Rs], L) :- sort_list(Rs, M), insert([A-X], M, L).
 
 
-%6
+% 6 - ​build_tree(List,Tree)
+% Predicado que usamos para transformar una lista de pares de elementos ordenados en
+% un árbol de Huffman.
+% Input : 	List - lista con los pares de elementos ordenador
+% 			Tree - árbol de Huffman obtenido.
 
+% Caso base: Una lista vacia no hay árbol
 build_tree([], nil).
+% Si la lista tiene un elemento insertamos en el arbol como hoja (con null en sus hijos)
+
 build_tree([A], tree(A, nil, nil)).
 build_tree([A-_|Rs], tree(1, X, M) ) :- Rs \= [], build_tree(Rs, M), X = tree(A, nil,nil).
 
@@ -128,7 +156,14 @@ encode_list([], [],_).
 % concatenamos dicha codificacion con A, siendo esto la lista de codificaciones resultante
 encode_list([X | Rs], Y, T) :- encode_list(Rs,A,T), encode_elem(X,R,T), concatena([R],A,Y).
 
-% 8
+% 8- encode(L1, L2)
+% Predicado que usamos para codificar la lista L1 en L2, usando el predicado diccionario
+% Input : 	L1 - lista con elementos a codificar
+%			L2 - lista resultado
+
+%Función diccionario
 dictionary([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]).
+% Caso base: Una lista vacia 
 encode([],nil).
+% Realizamos una llamada a todas las funciones anteriormente codificadas
 encode(L,X) :-dictionary(D),list_count(D, L, LC),sort_list(LC, SL),invierte(SL, I),build_tree(I, BT),encode_list(L, X, BT).
